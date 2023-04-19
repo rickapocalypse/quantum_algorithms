6349522b0d9aa35f0fbe8d43433a7ca5ca9c53f0
Henrique <rick.apocalypse@hotmail.com>
2023-04-19 11:21:35 -0300

    flipping bit add

--- coin_flipping_quantum/coin_flipping_bit.py
+++ coin_flipping_quantum/coin_flipping_bit.py
@@ -1,14 +1,15 @@
 from qiskit import *
 
+def bit_from_counts(counts):
+    return [k for k, v in counts.items() if v == 1][0]
+
 qc = QuantumCircuit(1,1)
 qc.h(0)
 qc.measure(0,0)
 
 backend = BasicAer.get_backend('qasm_simulator')
-job = execute(qc, backend, shots=1).result()
-count = job.data(qc)
 
-if count['counts'].get('0x0') == True:
-    print('Colapsou em 0')
-else:
-    print('Colapsou em 1')
+job = execute(qc, backend, shots=1).result()
+count = job.get_counts(qc)
+
+print(bit_from_counts(count))
